dcfb0ba57612f21a4185f517d24a8c008add11dc
tsubono <tsubono@wss.systems>
2020-12-02 15:07:26 +0900

    シーダー追加

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+		$user = DB::table('users')->where('name', 'admin')->first();
+        if (empty($user)) {
+            $now = \Carbon\Carbon::now();
+            DB::table('users')->insert([
+                    [
+                        'name' => 'admin',
+                        'handle_name' => 'admin',
+                        'email' => 'admin@test.com',
+                        'password' => \Hash::make('xTFR4u/*v8QB'),
+                        'admin_flg' => 1,
+                        'created_at' => $now,
+                        'updated_at' => $now,
+                    ]
+                ]
+
+            );
+        }
+	}
+}
