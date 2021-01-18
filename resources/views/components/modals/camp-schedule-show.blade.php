b799f8e608b496d01dd54b33c69e9fb258d0b638
tsubono <tsubono@wss.systems>
2021-01-19 01:53:05 +0900

    画像変更

--- resources/views/components/modals/camp-schedule-show.blade.php
+++ resources/views/components/modals/camp-schedule-show.blade.php
@@ -125,7 +125,7 @@
                         <label>画像</label>
                         <div class="item-value images">
                             @foreach ($campSchedule->images as $image)
-                                <img src="{{ $image->image_path }}" alt="画像" class="js-modal-image js-image-{{ $image->id }}" data-id="{{ $image->id }}" />
+                                <img src="{{ str_replace("camp-schedule/", "camp-schedule/resized-", $image->image_path) }}" alt="画像" class="js-modal-image js-image-{{ $image->id }}" data-id="{{ $image->id }}" />
                             @endforeach
                         </div>
                     </div>
