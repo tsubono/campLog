01481b125ccac7a143d8afb661723c5745847306
tsubono <tsubono@wss.systems>
2021-01-20 21:29:52 +0900

    アップローダー改修

--- resources/js/components/DropImages.vue
+++ resources/js/components/DropImages.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="file-area">
+        <div class="loader-container" v-if="isLoading">
+            <div class="loader">Loading</div>
+        </div>
         <div :class="{'drag': isDrag === 'new'}"
              @dragover.prevent="checkDrag($event, 'new', true)"
              @dragleave.prevent="checkDrag($event, 'new', false)"
@@ -23,8 +26,8 @@
                 <a v-on:click="onDelete(index)" class="delete-btn">削除する</a>
             </div>
         </div>
-        <div v-if="msg">
-            <span class="text-danger">{{ msg }}</span>
+        <div v-if="msg" class="error-txt">
+            <span>{{ msg }}</span>
         </div>
     </div>
 </template>
@@ -39,6 +42,7 @@
                 'msg': '',
                 'imgDatas': [],
                 'isDrag': null,
+                'isLoading': false,
             }
         },
         created: function () {
@@ -53,6 +57,7 @@
             },
             onDrop (event) {
                 this.isDrag = null;
+                this.isLoading = true;
                 this.msg = '';
                 let fileList = event.target.files || event.dataTransfer.files;
                 let files  = [];
@@ -62,14 +67,24 @@
                 // ファイルチェック
                 if (!files.length) {
                     this.msg = 'ファイル形式が不正です。';
+                    this.isLoading = false;
                     return false;
                 }
+                let totalSize = 0;
                 files.forEach (function(file) {
                     if (!file.type.match('image.*')) {
                         this.msg = 'ファイル形式が不正です。';
+                        this.isLoading = false;
                         return false;
                     }
+                    totalSize = totalSize + file.size;
                 });
+                // ファイルサイズチェック
+                if(totalSize > 8000000){
+                    this.msg = '一度にアップロードできる画像サイズの容量を超えました。';
+                    this.isLoading = false;
+                    return false;
+                }
 
                 let formData   = new FormData();
                 files.forEach (function(file) {
@@ -88,6 +103,10 @@
                     if (json.status === 'ok') {
                         self.imgDatas = self.imgDatas.concat(json.paths)
                     }
+                    self.isLoading = false;
+                }).catch((error) => {
+                    console.error('Error:', error);
+                    this.isLoading = false;
                 });
             },
             onDelete (index) {
@@ -98,6 +117,10 @@
 </script>
 
 <style lang="scss" scoped>
+    .error-txt {
+        color: #e88b79;
+        text-align: left;
+    }
     .drop-area {
         background: lightgray;
         width: 80%;
@@ -126,7 +149,7 @@
 
     .file-select-btn {
         cursor: pointer;
-        background: #799bd1;
+        background: #62b882;
         color: #fff;
         border: none;
         border-radius: 10px;
@@ -160,6 +183,87 @@
                 position: relative;
                 top: -27px;
             }
+        }
+    }
+
+    .loader-container {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        z-index: 1;
+        height: 100%;
+        left: 0;
+        background: #FFF;
+        opacity: 0.5;
+    }
+    .loader,
+    .loader:before,
+    .loader:after {
+        border-radius: 50%;
+    }
+    .loader {
+        color: #62b882;
+        font-size: 11px;
+        text-indent: -99999em;
+        margin: 55px auto;
+        position: relative;
+        width: 10em;
+        height: 10em;
+        box-shadow: inset 0 0 0 1em;
+        -webkit-transform: translateZ(0);
+        -ms-transform: translateZ(0);
+        transform: translateZ(0);
+        top: 30%;
+    }
+    .loader:before,
+    .loader:after {
+        position: absolute;
+        content: '';
+    }
+    .loader:before {
+        width: 5.2em;
+        height: 10.2em;
+        background: #FFF;
+        opacity: 0.5;
+        border-radius: 10.2em 0 0 10.2em;
+        top: -0.1em;
+        left: -0.1em;
+        -webkit-transform-origin: 5.1em 5.1em;
+        transform-origin: 5.1em 5.1em;
+        -webkit-animation: load2 2s infinite ease 1.5s;
+        animation: load2 2s infinite ease 1.5s;
+    }
+    .loader:after {
+        width: 5.2em;
+        height: 10.2em;
+        background: #FFF;
+        opacity: 0.5;
+        border-radius: 0 10.2em 10.2em 0;
+        top: -0.1em;
+        left: 4.9em;
+        -webkit-transform-origin: 0.1em 5.1em;
+        transform-origin: 0.1em 5.1em;
+        -webkit-animation: load2 2s infinite ease;
+        animation: load2 2s infinite ease;
+    }
+    @-webkit-keyframes load2 {
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+    }
+    @keyframes load2 {
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
         }
     }
 
