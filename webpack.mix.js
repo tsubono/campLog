7c5b8ab96c16f0fecba556738ad2f68b7263e4e0
tsubono <tsubono@wss.systems>
2021-07-04 02:56:15 +0900

    safariバグ対応

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js').vue()
   .sass('resources/sass/app.scss', 'public/css')
+  .options({ processCssUrls: false })
 
 if (mix.inProduction()) {
   mix.version()
